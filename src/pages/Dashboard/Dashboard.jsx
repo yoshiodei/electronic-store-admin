523b2394a5453052f62f0932d80490e97235c773
Yoshi Odei <yoshiodei@yahoo.com>
2023-06-30 22:45:35 +0000

    add fetch fuctionality

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,13 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { collection, getDocs } from 'firebase/firestore';
 import InfoPanels from './components/InfoPanels';
 import Header from '../components/Header';
 import DisplayGraphs from './components/DisplayGraphs';
+import { db } from '../../config/firebaseConfig';
 
 export default function Dashboard() {
+  const initialCount = {
+    vendorsCount: 0,
+    productsCount: 0,
+    pendingItemsCount: 0,
+    averageCount: 0,
+  };
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const vendorsSnap = await getDocs(collection(db, 'vendors'));
+      const pendingItemsSnap = await getDocs(collection(db, 'pendingItems'));
+      const productsSnap = await getDocs(collection(db, 'products'));
+      const vendorsCount = vendorsSnap.size;
+      const pendingItemsCount = pendingItemsSnap.size;
+      const productsCount = productsSnap.size;
+      setCount({
+        vendorsCount,
+        pendingItemsCount,
+        productsCount,
+        averageCount: (vendorsCount / productsCount),
+      });
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="page">
       <Header title="Dashboard" />
-      <InfoPanels />
+      <InfoPanels count={count} />
       <DisplayGraphs />
     </div>
 
