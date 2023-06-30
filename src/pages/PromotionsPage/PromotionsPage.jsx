b4db270589e2d3143d41efa64fcab356a409eb95
Yoshi Odei <yoshiodei@yahoo.com>
2023-06-30 22:48:46 +0000

    add promotion component

--- src/pages/PromotionsPage/PromotionsPage.jsx
+++ src/pages/PromotionsPage/PromotionsPage.jsx
@@ -5,6 +5,9 @@ export default function PromotionsPage() {
   return (
     <div className="page">
       <Header title="Promotions" />
+      <div className="promotion__empty-div">
+        <h1>Items are yet to be Promoted</h1>
+      </div>
     </div>
   );
 }
