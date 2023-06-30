42df9a214fba399972066661996c7f99db38cdd2
Yoshi Odei <yoshiodei@yahoo.com>
2023-06-30 04:07:53 +0000

    add empty tab component

---
+++ src/pages/SingleUserPage/components/EmptyTab.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function EmptyTab() {
+  return (
+    <div className="products-tab__content-empty">
+      {' '}
+      <h2>There are no items in this section</h2>
+      {' '}
+    </div>
+  );
+}
