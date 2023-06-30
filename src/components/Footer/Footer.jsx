01c39e8680c036ebeab39aa6a6e1096b7c93320b
Yoshi Odei <yoshiodei@yahoo.com>
2023-06-30 04:01:25 +0000

    add footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="footer">
-      <h5>Made with love by @Kvzu</h5>
+      {/* <h5>Made with love by @Kvzu</h5> */}
+      <h5 className="m-0">Tektoss</h5>
     </footer>
   );
 }
